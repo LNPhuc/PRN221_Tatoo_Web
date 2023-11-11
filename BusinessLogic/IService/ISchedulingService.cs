7f8f45ff1a3068ad2bc2e134ff8892c6e76816c3
zimbear3103 <kiendhduc@gmail.com>
2023-11-12 00:49:46 +0700

    add scheduling flow and Add validation for date

--- BusinessLogic/IService/ISchedulingService.cs
+++ BusinessLogic/IService/ISchedulingService.cs
@@ -1,6 +1,18 @@
-﻿namespace BusinessLogic.IService;
+﻿using DataAccess.DataAccess;
+
+namespace BusinessLogic.IService;
 
 public interface ISchedulingService
 {
+    IEnumerable<Scheduling> GetAll();
+    Scheduling GetById(Guid id);
+    Scheduling Update(Scheduling scheduling);
+    Scheduling Delete(Scheduling scheduling);
+    Scheduling Create(Scheduling scheduling);
+    void SaveChanges();
+    Customer GetCustomerByID(Guid id);
+    Account GetAccountByID(Guid id);
+    Booking GetBookingByID(Guid id);
+    void UpdateBooking(Booking booking);
     
 }
